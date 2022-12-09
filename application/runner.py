b4f92b1a42fc3b5f36089fb5c98d6896f365ab5f
Jan Holub <jan.holub@1pf.cz>
2022-12-09 17:46:16 +0100

    Func, if organization is in db.

--- application/runner.py
+++ application/runner.py
@@ -26,17 +26,68 @@ class CLI:
         self._db_pass = pass_line.split(' ')[1]
 
     def _load_org_info(self):
+
+        org_id = self._check_org(self._org_name)
         msg_type, data = self.gh_link.get_org_info()
 
         if msg_type == 'ok':
             data = json.loads(data)
-            for each in data:
-                print(f'Repo list: {each["name"]}')
+            col = 'name'
+            for table in data:
+                print(f'Repository name: {table[col]}')
+                not_in = self.db.db_table_check('repositories', col, table[col])
+                repo_id = self._check_repo(table[col], org_id)
+
+                if not_in:
+                    print(f'Repository is not in db.'
+                          f'\n[ {table[col]} ] added'
+                          f'\n- Table ID: {repo_id}'
+                          f'\n- Organization ID: {org_id}\n')
+                else:
+                    print(f'Repository [ {table[col]} ] is in db.\n')
         else:
             print(f'An error has occurred: {data}')
 
-    def test_run(self):
+    def _check_org(self, org_name):
+        col = 'name'
+        table_name = 'organizations'
+        not_in, table_len = self.db.db_table_check(table_name, col, org_name)
+
+        if not_in:
+            data = [org_name, 0]
+            self.db.add_to_table(table_name, data)
+
+            return table_len
+
+        else:
+            org_id = self.db.get_value(table_name, 'name', org_name, 'ID')
+
+            return org_id
+
+    def _check_repo(self, repo_name, org_id):
+        col = 'name'
+        table_name = 'repositories'
+        not_in, table_len = self.db.db_table_check(table_name, col, repo_name)
+
+        if not_in:
+            data = [repo_name, org_id]
+            self.db.add_to_table(table_name, data)
+
+            return table_len
+
+        else:
+            repo_id = self.db.get_value(table_name, 'name', repo_name, 'ID')
+
+            return repo_id
+
+    def _set_active_org(self):
+        # TODO : create function for setting active org
         pass
+
+    def test_run(self):
+        print()
+        for table in self.db.get_all_tables():
+            print(self.db.get_table(table[0]))
         # self.gh_link.check_status()
 
         ''' displays all tables and types
--- db/manager.py
+++ db/manager.py
@@ -5,8 +5,9 @@ from pysqlitecipher import sqlitewrapper
 
 class DBManager:
     def __init__(self, password):
+        self.password = password
         self._db_path = os.path.realpath('../db/pysqlitecipher.db')
-        self.db = sqlitewrapper.SqliteCipher(dataBasePath=self._db_path, checkSameThread=False, password=password)
+        self.db = sqlitewrapper.SqliteCipher(dataBasePath=self._db_path, checkSameThread=False, password=self.password)
 
     def new_table(self, table_name, table_data):
         self.db.createTable(table_name, table_data, makeSecure=True, commit=True)
@@ -27,9 +28,70 @@ class DBManager:
     def desc_table(self, table_name):
         return self.db.describeTable(table_name)
 
+    def db_table_check(self, table_name, col, value):
+        # TODO : if needed, add function for checking multiple variables
+        table_names, table_data = self.get_table(table_name)
+        not_in = True
+        for row in table_data:
+            if row[table_names.index(col)] == value:
+                not_in = False
 
+        return not_in, len(table_data)
 
+    def get_value(self, table_name, in_col, value, out_col):
+        table_names, table_data = self.get_table(table_name)
+        not_in = True
 
+        for row in table_data:
+            if row[table_names.index(in_col)] == value:
+                not_in = row[table_names.index(out_col)]
 
+        return not_in
 
+    def _gen_empty_db(self):
+        self._gen_org_table()
+        self._gen_repo_table()
+        self._gen_team_table()
+        self._gen_key_table()
+        self._gen_user_table()
 
+    def _gen_org_table(self):
+        table_name = 'organizations'
+        table_columns = [
+            ['name', 'TEXT'],
+            ['active', 'INT']
+        ]
+        self.new_table(table_name, table_columns)
+
+    def _gen_repo_table(self):
+        table_name = 'repositories'
+        table_columns = [
+            ['name', 'TEXT'],
+            ['org_id', 'INT']
+        ]
+        self.new_table(table_name, table_columns)
+
+    def _gen_team_table(self):
+        table_name = 'teams'
+        table_columns = [
+            ['name', 'TEXT'],
+            ['repo_id', 'INT'],
+            ['role', 'TEXT']
+        ]
+        self.new_table(table_name, table_columns)
+
+    def _gen_key_table(self):
+        table_name = 'users'
+        table_columns = [
+            ['name', 'TEXT'],
+            ['team_id', 'INT']
+        ]
+        self.new_table(table_name, table_columns)
+
+    def _gen_user_table(self):
+        table_name = 'keys'
+        table_columns = [
+            ['name', 'TEXT'],
+            ['repo_id', 'INT']
+        ]
+        self.new_table(table_name, table_columns)
